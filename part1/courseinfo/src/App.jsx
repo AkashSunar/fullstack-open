790df5e6d8e1180817165a04ac94b731fa3dbe8c
akash sunar <sunarakash1122@gmail.com>
2023-07-20 22:41:56 +0545

    part-1.4-completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,52 +1,42 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1.name}
-        exercises1={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
-      />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = ({ course }) => {
+  return <h1>{course} </h1>;
 };
 const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </>
-  );
+  return props.parts.map((val, index) => (
+    <Part part={val.name} exercises={val.exercises} key={index} />
+  ));
 };
 const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
-    </p>
-  );
+  let sum = 0;
+  props.parts.map((val) => {
+    sum = +val.exercises;
+  });
+  return <p>Number of exercises {sum}</p>;
 };
 
 const Part = (props) => {
