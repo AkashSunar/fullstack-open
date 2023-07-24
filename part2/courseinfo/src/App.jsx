56e44a61a05a08bd21a132698a58f7186e0f72f5
akash sunar <sunarakash1122@gmail.com>
2023-07-24 15:01:24 +0545

    part2.1-completed

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+      <Content course={course} />
+    </div>
+  );
+};
+const Course = (props) => {
+  return <h1>{props.course.name} </h1>;
+};
+const Content = (props) => {
+  return props.course.parts.map((val, index) => (
+    <Part part={val.name} exercises={val.exercises} key={index} />
+  ));
+};
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+export default App;
