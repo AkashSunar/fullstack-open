7598e6b54bf6d92c35c30ec1d9dd31257773abb7
akash sunar <sunarakash1122@gmail.com>
2023-07-26 09:54:13 +0545

    pjhonebook-2.8-completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [number, setNumber] = useState([{ number: 9833 }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,10 +15,16 @@ const App = () => {
     !personsArray.includes(newName)
       ? setPersons(persons.concat({ name: newName }))
       : alert(newName + " is already added to the phonebook");
+    setNumber(number.concat({ number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
+
   const handleChange = (event) => {
     setNewName(event.target.value);
+  };
+  const handleNum = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -27,13 +35,26 @@ const App = () => {
           name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNum} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map((value, index) => {
-        return <p key={index}>{value.name}</p>;
-      })}
+      <div>
+        <div>
+          {persons.map((value, index) => {
+            let num = number[index];
+            return (
+              <p key={index}>
+                {value.name} {num.number}
+              </p>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
