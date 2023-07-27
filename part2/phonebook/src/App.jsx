8a4d2eba7fb49572f55b8a0caaf5e24c4ee2807f
akash sunar <sunarakash1122@gmail.com>
2023-07-27 15:17:20 +0545

    phonebook-2.11-completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filterName, setfilterName] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    let myAxiosPromise = axios.get("http://localhost:3001/persons");
+    myAxiosPromise.then((myResult) => {
+      setPersons(myResult.data);
+    });
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
