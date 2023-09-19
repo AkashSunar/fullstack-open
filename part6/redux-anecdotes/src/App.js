89096570d6d9f1bf567de8bcefb31c0f8e8212bc
akash sunar <sunarakash1122@gmail.com>
2023-09-19 07:04:20 +0545

    part 6.5 completed

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,7 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { getId } from "./reducers/anecdoteReducer";
 const App = () => {
-  const anecdotes = useSelector((state) => state);
+const anecdotes = useSelector((state) => {
+  return state.sort((val1, val2) => {
+    return val2.votes - val1.votes;
+  });
+});
   const dispatch = useDispatch();
 
   const vote = (id) => {
