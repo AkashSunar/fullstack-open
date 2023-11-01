d5d4286dc26f456e8142fdd3b5dafe146b197766
akash sunar <sunarakash1122@gmail.com>
2023-11-01 14:13:41 +0545

    part 13.6 completed

--- part13/blog-app/controllers/blogs.js
+++ part13/blog-app/controllers/blogs.js
@@ -14,6 +14,20 @@ app.post("/", async (req, res) => {
     res.status(500).send("error occured");
   }
 });
+app.put("/:id", async (req, res, next) => {
+  const blog = await Blog.findByPk(req.params.id);
+  try {
+    if (blog) {
+      blog.likes += 1;
+      await blog.save();
+      res.status(200).json({ likes: blog.likes })
+    } else {
+      throw new Error("NOT FOUND")
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.delete("/:id", async (req, res) => {
   const id = req.params.id;
