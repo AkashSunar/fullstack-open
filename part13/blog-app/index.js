53800477f04985a7aca949740c03b7b31f355704
akash sunar <sunarakash1122@gmail.com>
2023-11-02 00:20:01 +0545

    part 13.8 completed

--- part13/blog-app/index.js
+++ part13/blog-app/index.js
@@ -7,12 +7,16 @@ const { PORT } = require("./util/config")
 const { connectToDatabase } = require("./util/db") 
 
 const blogsRouter = require("./controllers/blogs")
+const loginRouter = require("./controllers/login")
+const userRouter=require("./controllers/users")
 const { errorHandler,unknownEndpoint } = require("./util/middleware")
 
 
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter)
+app.use("/api/login", loginRouter)
+app.use("/api/users", userRouter);
 
 app.use(errorHandler)
 app.use(unknownEndpoint);
--- part13/blog-app/util/config.js
+++ part13/blog-app/util/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 module.exports = {
     DB_URL: process.env.DB_URL,
-    PORT:process.env.PORT
+    PORT: process.env.PORT,
+    SECRET:process.env.SECRET
 
 }
